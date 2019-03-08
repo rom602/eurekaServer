127dacd890195a98a6ada3a5b39fa8b6f69a3fc2
강현지 <khj1219@midasit.com>
2019-03-08 18:49:15 +0900

    Spring Boot Test - 코드 정리

--- src/test/java/com/wari/eurekaServer/UserTests.java
+++ src/test/java/com/wari/eurekaServer/UserTests.java
@@ -27,23 +27,20 @@ public class UserTests {
         User user = new User("user", "Jack", "Frost", "jfrost@example.com");
         user.setId(0L);
         user.setCreatedAt(12345L);
-        user.setLastModified(12345L);
+        user.setLastModified(12346L);
         this.user = user;
     }
 
     @Test
     public void deserializeJson() throws Exception {
         String content = "{\n" +
-                "  \"username\": \"user\",\n" +
-                "  \"firstName\": \"Jack\",\n" +
-                "  \"lastName\": \"Frost\",\n" +
-                "  \"email\": \"jfrost@example.com\",\n" +
-                "  \"createdAt\": 12345,\n" +
-                "  \"lastModified\": 12346,\n" +
-                "  \"id\": 0\n" +
+                "  \"username\":\"user\",\n" +
+                "  \"firstName\":\"Jack\",\n" +
+                "  \"lastName\":\"Frost\",\n" +
+                "  \"email\":\"jfrost@example.com\"\n" +
                 "}";
-        /*assertThat(this.json.parse(content))
-                .isEqualTo(new User("user", "Jack", "Frost", "jfrost@example.com"));*/
+        assertThat(this.json.parse(content))
+                .isEqualTo(new User("user", "Jack", "Frost", "jfrost@example.com"));
         assertThat(this.json.parseObject(content).getUsername())
                 .isEqualTo("user");
     }
@@ -51,8 +48,8 @@ public class UserTests {
     @Test
     public void serializeJson() throws Exception {
         // User 객체를 JSON으로 변환하고, user.json 파일과 비교한다.
-        /*assertThat(this.json.write(user)).isEqualTo("user.json");
-        assertThat(this.json.write(user)).isEqualToJson("user.json");*/
+        assertThat(this.json.write(user)).isEqualTo("user.json");
+        assertThat(this.json.write(user)).isEqualToJson("user.json");
         assertThat(this.json.write(user)).hasJsonPathStringValue("@.username");
 
         // 실제 JSON 결과가 예상한 속성값과 일치하는지 판정한다.
